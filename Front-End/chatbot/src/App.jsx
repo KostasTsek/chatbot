592fc750ec02738ceb1884c7ace3261aef45e73b
KostasTsek <kostistsek04@gmail.com>
2025-05-28 23:02:56 +0300

    Front-End skeleton

--- Front-End/chatbot/src/App.jsx
+++ Front-End/chatbot/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import sendIcon from './assets/arrow_upward.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function MessageBox() {
+  return(
+    <div className='messageBox'>
+      <div className='content'></div>
+    </div>
+    );
+}
 
+function InputBox() {
+  return(
+    <div className='chat-input-container'>
+      <input className='chat-input' type='text' placeholder='Type anything.'/>
+      <button className="send-button"><img className='send_icon' src={sendIcon} alt='send'/></button>
+    </div>
+  );
+}
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <header className='header-container'><h1>Chatbot</h1><hr/></header>
+      <div className='message-container'>
+        <MessageBox/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <InputBox/>
+    </div>
+  );
 }
 
 export default App
