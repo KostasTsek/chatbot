b0cb593c77e9ab5eadac4f308a258d4620e585c7
KostasTsek <kostistsek04@gmail.com>
2025-05-29 04:04:47 +0300

    Finished styling

--- Front-End/chatbot/src/App.jsx
+++ Front-End/chatbot/src/App.jsx
@@ -6,11 +6,15 @@ function MessageBox({ sender, text }) {
   const isUser = sender === 'user';
 
   return(
-    <div className='messageBox'>
-      {!isUser && (
-        <img src={botAvatar} alt='bot' className='bot-Avatar'/>
-      )}
-      <div className='content'>{text}</div>
+    <div className={`message-wrapper ${isUser ? "user" : "bot"}`}>
+      <div className={`messageBox ${isUser ? "user" : "bot"}`}>
+        {!isUser && (
+          <div className='bot-Avatar'>
+            <img src={botAvatar} alt='bot' width='30vw' height='30vh'/>
+          </div>
+        )}
+        <div className={`content ${isUser ? "user" : "bot"}`}>{text}</div>
+      </div>
     </div>
     );
 }
@@ -19,21 +23,21 @@ function InputBox() {
   return(
     <div className='chat-input-container'>
       <input className='chat-input' type='text' placeholder='Type anything.'/>
-      <button className="send-button"><img className='send_icon' src={sendIcon} alt='send'/></button>
+      <button className="send-button"><img className='send_icon' src={sendIcon} alt='send' width='20vw' height='20vh'/></button>
     </div>
   );
 }
 
 function App() {
   return (
-    <div>
+    <>
       <header className='header-container'><h1>Chatbot</h1><hr/></header>
       <div className='message-container'>
         <MessageBox sender='user' text='Question'/>
-        <MessageBox sender='bot' text='Answer'/>
+        <MessageBox sender='bot' text='aaaaaaaaaaaaaaaaaaaaaaaaaaaa woooord nicce words'/>
       </div>
       <InputBox/>
-    </div>
+    </>
   );
 }
 
